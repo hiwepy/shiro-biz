807b231ef43862e0317901e7ac2651847dfb6947
vindell <hnxyhcwdl1003@163.com>
2017-09-16 23:13:38 +0800

    sx

---
+++ src/main/java/org/apache/shiro/biz/web/filter/authc/KickoutSessionControlFilter.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2010-2020, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.shiro.biz.web.filter.authc;
+
+import java.io.Serializable;
+import java.util.Deque;
+import java.util.LinkedList;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.mgt.DefaultSessionKey;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.AccessControlFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+/**
+ * 
+ *  cacheManager：使用cacheManager获取相应的cache来缓存用户登录的会话；用于保存用户—会话之间的关系的；
+	sessionManager：用于根据会话ID，获取会话进行踢出操作的；
+	kickoutAfter：是否踢出后来登录的，默认是false；即后者登录的用户踢出前者登录的用户；
+	maxSession：同一个用户最大的会话数，默认1；比如2的意思是同一个用户允许最多同时两个人登录；
+	kickoutUrl：被踢出后重定向到的地址；
+ * 
+ * @className	： KickoutSessionControlFilter
+ * @description	： TODO(描述这个类的作用)
+ * @author 		： <a href="https://github.com/vindell">vindell</a>
+ * @date		： 2017年9月16日 下午11:02:46
+ * @version 	V1.0
+ */
+public class KickoutSessionControlFilter extends AccessControlFilter {
+
+    private String kickoutUrl; //踢出后到的地址
+    private boolean kickoutAfter = false; //踢出之前登录的/之后登录的用户 默认踢出之前登录的用户
+    private int maxSession = 1; //同一个帐号最大会话数 默认1
+
+    private SessionManager sessionManager;
+    private Cache<String, Deque<Serializable>> cache;
+
+    public void setKickoutUrl(String kickoutUrl) {
+        this.kickoutUrl = kickoutUrl;
+    }
+
+    public void setKickoutAfter(boolean kickoutAfter) {
+        this.kickoutAfter = kickoutAfter;
+    }
+
+    public void setMaxSession(int maxSession) {
+        this.maxSession = maxSession;
+    }
+
+    public void setSessionManager(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    public void setCacheManager(CacheManager cacheManager) {
+        this.cache = cacheManager.getCache("shiro-kickout-session");
+    }
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        Subject subject = getSubject(request, response);
+        if(!subject.isAuthenticated() && !subject.isRemembered()) {
+            //如果没有登录，直接进行之后的流程
+            return true;
+        }
+
+        Session session = subject.getSession();
+        String username = (String) subject.getPrincipal();
+        Serializable sessionId = session.getId();
+
+        //TODO 同步控制
+        Deque<Serializable> deque = cache.get(username);
+        if(deque == null) {
+            deque = new LinkedList<Serializable>();
+            cache.put(username, deque);
+        }
+
+        //如果队列里没有此sessionId，且用户没有被踢出；放入队列
+        if(!deque.contains(sessionId) && session.getAttribute("kickout") == null) {
+            deque.push(sessionId);
+        }
+
+        //如果队列里的sessionId数超出最大会话数，开始踢人
+        while(deque.size() > maxSession) {
+            Serializable kickoutSessionId = null;
+            if(kickoutAfter) { //如果踢出后者
+                kickoutSessionId = deque.removeFirst();
+            } else { //否则踢出前者
+                kickoutSessionId = deque.removeLast();
+            }
+            try {
+                Session kickoutSession = sessionManager.getSession(new DefaultSessionKey(kickoutSessionId));
+                if(kickoutSession != null) {
+                    //设置会话的kickout属性表示踢出了
+                    kickoutSession.setAttribute("kickout", true);
+                }
+            } catch (Exception e) {//ignore exception
+            }
+        }
+
+        //如果被踢出了，直接退出，重定向到踢出后的地址
+        if (session.getAttribute("kickout") != null) {
+            //会话被踢出了
+            try {
+                subject.logout();
+            } catch (Exception e) { //ignore
+            }
+            saveRequest(request);
+            WebUtils.issueRedirect(request, response, kickoutUrl);
+            return false;
+        }
+
+        return true;
+    }
+}
