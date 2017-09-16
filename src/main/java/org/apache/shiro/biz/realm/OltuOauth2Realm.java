1e467e1c13059f0ea0520928a83b3a3eb239c3ad
vindell <hnxyhcwdl1003@163.com>
2017-09-16 22:59:30 +0800

    xx

---
+++ src/main/java/org/apache/shiro/biz/realm/OltuOauth2Realm.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2010-2020, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.shiro.biz.realm;
+
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthBearerClientRequest;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAccessTokenResponse;
+import org.apache.oltu.oauth2.client.response.OAuthResourceResponse;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.biz.authc.exception.OAuth2AuthenticationException;
+import org.apache.shiro.biz.authc.token.OltuOauth2Token;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class OltuOauth2Realm extends AuthorizingRealm {
+
+    private String clientId;
+    private String clientSecret;
+    private String accessTokenUrl;
+    private String userInfoUrl;
+    private String redirectUrl;
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public void setAccessTokenUrl(String accessTokenUrl) {
+        this.accessTokenUrl = accessTokenUrl;
+    }
+
+    public void setUserInfoUrl(String userInfoUrl) {
+        this.userInfoUrl = userInfoUrl;
+    }
+
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof OltuOauth2Token;//表示此Realm只支持OAuth2Token类型
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        return authorizationInfo;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        OltuOauth2Token oAuth2Token = (OltuOauth2Token) token;
+        String code = oAuth2Token.getAuthCode();
+        String username = extractUsername(code);
+
+        SimpleAuthenticationInfo authenticationInfo =
+                new SimpleAuthenticationInfo(username, code, getName());
+        return authenticationInfo;
+    }
+
+    private String extractUsername(String code) {
+
+        try {
+            OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+
+            OAuthClientRequest accessTokenRequest = OAuthClientRequest
+                    .tokenLocation(accessTokenUrl)
+                    .setGrantType(GrantType.AUTHORIZATION_CODE)
+                    .setClientId(clientId)
+                    .setClientSecret(clientSecret)
+                    .setCode(code)
+                    .setRedirectURI(redirectUrl)
+                    .buildQueryMessage();
+
+            OAuthAccessTokenResponse oAuthResponse = oAuthClient.accessToken(accessTokenRequest, OAuth.HttpMethod.POST);
+
+            String accessToken = oAuthResponse.getAccessToken();
+            Long expiresIn = oAuthResponse.getExpiresIn();
+
+            OAuthClientRequest userInfoRequest = new OAuthBearerClientRequest(userInfoUrl)
+                    .setAccessToken(accessToken).buildQueryMessage();
+
+            OAuthResourceResponse resourceResponse = oAuthClient.resource(userInfoRequest, OAuth.HttpMethod.GET, OAuthResourceResponse.class);
+            String username = resourceResponse.getBody();
+            return username;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new OAuth2AuthenticationException(e);
+        }
+    }
+}
+
