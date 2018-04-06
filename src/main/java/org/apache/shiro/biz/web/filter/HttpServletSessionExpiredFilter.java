2ab8e03b11cd4ed0986bc116335858cd02fa5569
jeebiz <hnxyhcwdl1003@163.com>
2018-04-06 23:49:00 +0800

    会话超时

--- src/main/java/org/apache/shiro/biz/web/filter/HttpServletSessionExpiredFilter.java
+++ src/main/java/org/apache/shiro/biz/web/filter/HttpServletSessionExpiredFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.biz.utils.WebUtils;
 import org.apache.shiro.biz.web.servlet.http.HttpStatus;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.AccessControlFilter;
@@ -20,11 +21,11 @@ public class HttpServletSessionExpiredFilter extends AccessControlFilter {
 	public boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
 
 		Subject subject = SecurityUtils.getSubject();
-
 		if (subject == null || !subject.isAuthenticated()) {
-			if ("XMLHttpRequest".equalsIgnoreCase(((HttpServletRequest) request).getHeader("X-Requested-With"))) {
+			if (WebUtils.isAjaxRequest(request)) {
 				HttpServletResponse httpResponse = (HttpServletResponse) response;
 				httpResponse.setStatus(HttpStatus.SC_SESSION_TIMEOUT);
+				httpResponse.getWriter().write("session timeout.");
 				return false;
 			}
 		}
