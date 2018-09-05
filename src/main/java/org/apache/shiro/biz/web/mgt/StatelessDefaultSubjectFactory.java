05d868e3a07b1f93c3a14758feacad6fc79b28c5
vindell <hnxyhcwdl1003@163.com>
2018-09-05 22:19:19 +0800

    修改

--- src/main/java/org/apache/shiro/biz/web/mgt/StatelessDefaultSubjectFactory.java
+++ src/main/java/org/apache/shiro/biz/web/mgt/StatelessDefaultSubjectFactory.java
@@ -21,32 +21,39 @@ import org.apache.shiro.subject.SubjectContext;
 import org.apache.shiro.web.mgt.DefaultWebSubjectFactory;
 
 public class StatelessDefaultSubjectFactory extends DefaultWebSubjectFactory {
-	
+
 	private final DefaultSessionStorageEvaluator storageEvaluator;
 	/**
 	 * If Session Stateless
 	 */
-	private boolean stateless = false;
-	
+	private final boolean stateless;
+
 	/**
-	 * DefaultSessionStorageEvaluator是否持久化SESSION的开关 
+	 * DefaultSessionStorageEvaluator是否持久化SESSION的开关
 	 */
-	public StatelessDefaultSubjectFactory(DefaultSessionStorageEvaluator storageEvaluator,  boolean stateless){
+	public StatelessDefaultSubjectFactory(DefaultSessionStorageEvaluator storageEvaluator, boolean stateless) {
 		this.storageEvaluator = storageEvaluator;
 		this.stateless = stateless;
 	}
+
+	public Subject createSubject(SubjectContext context) {
+		storageEvaluator.setSessionStorageEnabled(Boolean.TRUE);
+		context.setSessionCreationEnabled(true);
+		if (stateless) {
+			// 不创建 session
+			context.setSessionCreationEnabled(Boolean.FALSE);
+			// 不持久化session
+			storageEvaluator.setSessionStorageEnabled(Boolean.FALSE);
+		}
+		return super.createSubject(context);
+	}
+
+	public DefaultSessionStorageEvaluator getStorageEvaluator() {
+		return storageEvaluator;
+	}
+
+	public boolean isStateless() {
+		return stateless;
+	}
 	
-	
-   public Subject createSubject(SubjectContext context) { 
-   	storageEvaluator.setSessionStorageEnabled(Boolean.TRUE);
-   	context.setSessionCreationEnabled(true);
-   	if(stateless){
-           // 不创建 session 
-           context.setSessionCreationEnabled(Boolean.FALSE);
-           // 不持久化session
-           storageEvaluator.setSessionStorageEnabled(Boolean.FALSE);
-   	}
-       return super.createSubject(context); 
-   }
-   
 }
