70756435ca97a631f29b92faa46c0b716d738f53
vindell <hnxyhcwdl1003@163.com>
2018-09-22 21:32:39 +0800

    修改

--- src/main/java/org/apache/shiro/biz/web/filter/HttpServletSessionControlFilter.java
+++ src/main/java/org/apache/shiro/biz/web/filter/HttpServletSessionControlFilter.java
@@ -23,7 +23,7 @@ import org.apache.shiro.web.util.WebUtils;
  */
 public abstract class HttpServletSessionControlFilter extends AccessControlFilter {
 
-	private static final String DEFAULT_SESSION_CONTROL_CACHE_NAME = "SHIRO_SESSION_CONTROL_CACHE";
+	public static final String DEFAULT_SESSION_CONTROL_CACHE_NAME = "SHIRO_SESSION_CONTROL_CACHE";
 	
 	/**
 	 * 用户sessionControl的缓存
