dc01025ac56be319aba0106daca4e90443879fe7
vindell <hnxyhcwdl1003@163.com>
2018-11-09 21:02:53 +0800

    修改

--- src/main/java/org/apache/shiro/biz/web/filter/HttpServletShiroFilter.java
+++ src/main/java/org/apache/shiro/biz/web/filter/HttpServletShiroFilter.java
@@ -1,22 +1,16 @@
 package org.apache.shiro.biz.web.filter;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-
-import org.apache.shiro.subject.Subject;
-import org.apache.shiro.util.ThreadContext;
 import org.apache.shiro.web.servlet.AbstractShiroFilter;
-import org.apache.shiro.web.subject.WebSubject;
 
 public class HttpServletShiroFilter extends AbstractShiroFilter {
 	
-	@Override
+	/*@Override
 	protected WebSubject createSubject(ServletRequest request, ServletResponse response) {
 		Subject subject = super.createSubject(request, response);
         if (subject == null) {
             ThreadContext.bind(subject);
         }
         return (WebSubject) subject;
-	}
+	}*/
 	
 }
