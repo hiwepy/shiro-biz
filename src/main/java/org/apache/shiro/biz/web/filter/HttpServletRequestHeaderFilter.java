14ec077c2e4153c4ef4dba7eab1a7c9c667a0bd6
wandl <hnxyhcwdl1003@163.com>
2019-11-09 13:43:31 +0800

    修改

--- src/main/java/org/apache/shiro/biz/web/filter/HttpServletRequestHeaderFilter.java
+++ src/main/java/org/apache/shiro/biz/web/filter/HttpServletRequestHeaderFilter.java
@@ -15,6 +15,8 @@
  */
 package org.apache.shiro.biz.web.filter;
 
+import java.util.Optional;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -52,42 +54,73 @@ public class HttpServletRequestHeaderFilter extends AccessControlFilter {
 	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue)
 			throws Exception {
 		
-		
-		
-		
 		HttpServletRequest httpRequest = WebUtils.toHttp(request);
 		HttpServletResponse httpResponse = WebUtils.toHttp(response);
 		
-		// 服务器端 Access-Control-Allow-Credentials = true时，参数Access-Control-Allow-Origin 的值不能为 '*' 
-		this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, Boolean.toString(properties.isAccessControlAllowCredentials()));
-		this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, properties.getAccessControlAllowHeaders());
-		this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, properties.getAccessControlAllowMethods());
-		this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, properties.getAccessControlAllowOrigin());
-		this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, properties.getAccessControlExposeHeaders());
-		this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_MAX_AGE, properties.getAccessControlMaxAge());
-		this.setHeader(httpResponse, HttpHeaders.CACHE_CONTROL, properties.getCacheControl());
-		this.setHeader(httpResponse, HttpHeaders.X_CONTENT_SECURITY_POLICY, properties.getContentSecurityPolicy());
-		this.setHeader(httpResponse, HttpHeaders.X_CONTENT_SECURITY_POLICY_REPORT_ONLY, properties.getContentSecurityPolicyReportOnly());
-		this.setHeader(httpResponse, FEATURE_POLICY_KEY, properties.getReferrerPolicy());
-		this.setHeader(httpResponse, HttpHeaders.REFERRER_POLICY, properties.getFeaturePolicy());
-		this.setHeader(httpResponse, HttpHeaders.STRICT_TRANSPORT_SECURITY, properties.getStrictTransportSecurity());
-		this.setHeader(httpResponse, HttpHeaders.TIMING_ALLOW_ORIGIN, properties.getTimingAllowOrigin());
+		// 服务器端 Access-Control-Allow-Credentials = true时，参数Access-Control-Allow-Origin 的值不能为 '*'
+		Optional.ofNullable(properties.isAccessControlAllowCredentials()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, Boolean.toString(properties.isAccessControlAllowCredentials()));
+		});
+		Optional.ofNullable(properties.getAccessControlAllowHeaders()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, properties.getAccessControlAllowHeaders());
+		});
+		Optional.ofNullable(properties.getAccessControlAllowMethods()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, properties.getAccessControlAllowMethods());
+		});
+		Optional.ofNullable(properties.getAccessControlAllowOrigin()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, properties.getAccessControlAllowOrigin());
+		});
+		Optional.ofNullable(properties.getAccessControlExposeHeaders()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, properties.getAccessControlExposeHeaders());
+		});
+		Optional.ofNullable(properties.getAccessControlMaxAge()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.ACCESS_CONTROL_MAX_AGE, properties.getAccessControlMaxAge());
+		});
+		Optional.ofNullable(properties.getCacheControl()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.CACHE_CONTROL, properties.getCacheControl());
+		});
+		Optional.ofNullable(properties.getContentSecurityPolicy()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.X_CONTENT_SECURITY_POLICY, properties.getContentSecurityPolicy());
+		});
+		Optional.ofNullable(properties.getContentSecurityPolicyReportOnly()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.X_CONTENT_SECURITY_POLICY_REPORT_ONLY, properties.getContentSecurityPolicyReportOnly());
+		});
+		Optional.ofNullable(properties.getReferrerPolicy()).ifPresent(value -> {
+			this.setHeader(httpResponse, FEATURE_POLICY_KEY, properties.getReferrerPolicy());
+		});
+		Optional.ofNullable(properties.getFeaturePolicy()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.REFERRER_POLICY, properties.getFeaturePolicy());
+		});
+		Optional.ofNullable(properties.getStrictTransportSecurity()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.STRICT_TRANSPORT_SECURITY, properties.getStrictTransportSecurity());
+		});
+		Optional.ofNullable(properties.getTimingAllowOrigin()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.TIMING_ALLOW_ORIGIN, properties.getTimingAllowOrigin());
+		});
 		//防止在IE9、chrome和safari中的MIME类型混淆攻击
-		this.setHeader(httpResponse, HttpHeaders.X_CONTENT_TYPE_OPTIONS, properties.getXContentTypeOptions());
-		this.setHeader(httpResponse, HttpHeaders.X_DNS_PREFETCH_CONTROL, properties.getXDnsPrefetchControl());
+		Optional.ofNullable(properties.getTimingAllowOrigin()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.X_CONTENT_TYPE_OPTIONS, properties.getXContentTypeOptions());
+		});
+		Optional.ofNullable(properties.getXDnsPrefetchControl()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.X_DNS_PREFETCH_CONTROL, properties.getXDnsPrefetchControl());
+		});
 		//iframe策略
-		this.setHeader(httpResponse, HttpHeaders.X_FRAME_OPTIONS, properties.getXFrameOptions());
+		Optional.ofNullable(properties.getXFrameOptions()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.X_FRAME_OPTIONS, properties.getXFrameOptions());
+		});
 		//X-XSS-Protection：主要是用来防止浏览器中的反射性xss，IE，chrome和safari（webkit）支持这个header,有以下两种方式：
 		//1; mode=block – 开启xss防护并通知浏览器阻止而不是过滤用户注入的脚本；
 		//1; report=http://site.com/report – 这个只有chrome和webkit内核的浏览器支持，这种模式告诉浏览器当
 		//发现疑似xss攻击的时候就将这部分数据post到指定地址。
-		this.setHeader(httpResponse, HttpHeaders.X_XSS_PROTECTION, properties.getXXssProtection());
+		Optional.ofNullable(properties.getXXssProtection()).ifPresent(value -> {
+			this.setHeader(httpResponse, HttpHeaders.X_XSS_PROTECTION, properties.getXXssProtection());
+		});
 		
 		// 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
 		if (httpRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
 			httpResponse.setStatus(HttpServletResponse.SC_OK);
-			return false;
 		}
+		
 		return true;
 	}
 	
@@ -105,6 +138,8 @@ public class HttpServletRequestHeaderFilter extends AccessControlFilter {
 
 	@Override
 	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+		
+		
 		return true;
 	}
 	
