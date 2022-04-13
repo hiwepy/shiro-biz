37bdc9ccfda67d7c59d27a72175bd49c89eac36e
wandl <hnxxyhcwdl1003@163.com>
2022-04-14 00:17:21 +0800

    优化

---
+++ src/main/java/org/apache/shiro/biz/cache/caffeine/CaffeineCacheManager.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.shiro.biz.cache.caffeine;
+
+
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.apache.shiro.cache.AbstractCacheManager;
+import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.util.Destroyable;
+
+public class CaffeineCacheManager extends AbstractCacheManager implements Destroyable {
+
+	protected LoadingCache<String, Object> cache;
+
+	public CaffeineCacheManager() {
+	}
+
+	public CaffeineCacheManager(LoadingCache<String, Object> cache) {
+		this.cache = cache;
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		if (cache != null) {
+			this.cache.invalidateAll();;
+		}
+	}
+
+	@Override
+	protected CaffeineCacheWrapper<Object> createCache(String name) throws CacheException {
+		return new CaffeineCacheWrapper<Object>(this.cache);
+	}
+
+}
---
+++ src/main/java/org/apache/shiro/biz/cache/caffeine/CaffeineCacheWrapper.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.shiro.biz.cache.caffeine;
+
+
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Caffeine Cache Wrapper
+ */
+public class CaffeineCacheWrapper<V> implements Cache<String, V> {
+
+	protected LoadingCache<String, V> cache;
+
+	public CaffeineCacheWrapper(LoadingCache<String, V> cache) {
+		this.cache = cache;
+	}
+
+	@Override
+	public V get(String key) throws CacheException {
+		try {
+			return (V) this.cache.get(key);
+		} catch (Exception e) {
+			throw new CacheException(e);
+		}
+	}
+
+	@Override
+	public V put(String key, V value) throws CacheException {
+		this.cache.put(key, value);
+		return value;
+	}
+
+	@Override
+	public V remove(String key) throws CacheException {
+		this.cache.invalidate(key);
+		return null;
+	}
+
+	@Override
+	public void clear() throws CacheException {
+		this.cache.invalidateAll();
+	}
+
+	@Override
+	public int size() {
+		return Long.valueOf(this.cache.estimatedSize()).intValue();
+	}
+
+	@Override
+	public Set<String> keys() {
+		return this.cache.asMap().keySet();
+	}
+
+	@Override
+	public Collection<V> values() {
+		return this.cache.asMap().values();
+	}
+
+}
